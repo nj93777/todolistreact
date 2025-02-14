53d8fce4f3ceeed1f6c281f6df9a6706165e1564
Nurmi Johnny <s2400330@BC-T2YG2MVD9H-Kannettava-tietokone.local>
2025-02-14 10:31:03 +0200

    first

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/todolistreact/',
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
+})
